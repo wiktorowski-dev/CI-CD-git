ca4f98a40fa3d2050a2c068b7ea4b2c6a140193c
Kamil <wiktorowski.dev@gmail.com>
2020-12-21 19:41:36 +0100

    Adding         os.system('git reset --hard HEAD')         repo.remotes.origin.pull()

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import time
 import subprocess
+import git
 
 
 class GitPuller(object):
@@ -10,15 +11,16 @@ class GitPuller(object):
 
     def __main_work_flow(self):
         while True:
-            time.sleep(15)
+            # time.sleep(15)
             paths = self.__find_git_directories()
             [self.__manage_update(x) for x in paths]
 
     @staticmethod
     def __find_git_directories():
-        paths = subprocess.run(['find', '/', '-type', 'd', '-name', '.git'], stdout=subprocess.PIPE)
-        paths = paths.stdout.decode('UTF-8')
-        paths = paths.split('\n')
+        # paths = subprocess.run(['find', '/', '-type', 'd', '-name', '.git'], stdout=subprocess.PIPE)
+        # paths = paths.stdout.decode('UTF-8')
+        # paths = paths.split('\n')
+        paths = [r'C:\Users\Kamil\PycharmProjects\CI-CD-git\.git']
         paths = [x for x in paths if x]
         return paths
 
@@ -26,7 +28,13 @@ class GitPuller(object):
     def __manage_update(path):
         path = os.path.split(path)[0]
         os.chdir(path)
-        os.system('git pull')
+        repo = git.Repo(os.getcwd())
+        if repo.is_dirty(untracked_files=True):
+            os.system('git reset --hard HEAD')
+            repo.remotes.origin.pull()
+        # output = subprocess.check_output(["git", "pull"])
+        # z = os.system('git pull')
+        print(23)
 
 
 if __name__ == '__main__':
