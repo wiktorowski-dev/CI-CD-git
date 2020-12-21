2a5f2f239a71ee08013f721e869a135155f58b66
Kamil <wiktorowski.dev@gmail.com>
2020-12-21 19:47:53 +0100

    New conception for the pulling
    - Using git reset --hard HEAD for make 100% possibility to pull

--- main.py
+++ main.py
@@ -11,16 +11,15 @@ class GitPuller(object):
 
     def __main_work_flow(self):
         while True:
-            # time.sleep(15)
+            time.sleep(15)
             paths = self.__find_git_directories()
             [self.__manage_update(x) for x in paths]
 
     @staticmethod
     def __find_git_directories():
-        # paths = subprocess.run(['find', '/', '-type', 'd', '-name', '.git'], stdout=subprocess.PIPE)
-        # paths = paths.stdout.decode('UTF-8')
-        # paths = paths.split('\n')
-        paths = [r'C:\Users\Kamil\PycharmProjects\CI-CD-git\.git']
+        paths = subprocess.run(['find', '/', '-type', 'd', '-name', '.git'], stdout=subprocess.PIPE)
+        paths = paths.stdout.decode('UTF-8')
+        paths = paths.split('\n')
         paths = [x for x in paths if x]
         return paths
 
@@ -32,8 +31,6 @@ class GitPuller(object):
         if repo.is_dirty(untracked_files=True):
             os.system('git reset --hard HEAD')
             repo.remotes.origin.pull()
-        # output = subprocess.check_output(["git", "pull"])
-        # z = os.system('git pull')
         print(23)
 
 
